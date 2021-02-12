840abcf5547be5d197048a6a25ed0e8e6b38793f
zeel-codder <zeelprajapati321@gmial.com>
2021-02-12 11:18:06 +0530

    addheader

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import logo from './img/1.jpg';
+// import logo from './img/1.jpg';
 import Navbar from './Halpper/navabar';
 import './App.css';
+import logo from './img/logo.jpg'
+import Imgeshow from './Halpper/imgshow'
+import ImgeShow from './Halpper/imgshow';
+import imgdic from './Data/Imgedata'
 
 function App() {
   return (
     <>
     <header>
-      <h2>
+      <img className="logo" src={logo}></img>
+      <span className="logo-text">
         Zeel Prajapati's Personal Web site
-      </h2>
+      </span>
     </header>
     <Navbar />
+      <ImgeShow imgdic={imgdic}></ImgeShow>
     </>
   );
 }
---
+++ src/Data/Imgedata.js
@@ -0,0 +1,27 @@
+import logo1 from '../img/1.jpg'
+import logo2 from '../img/2.jpg'
+import logo3 from '../img/3.jpg'
+import logo4 from '../img/4.jpg'
+
+const imgdata=[
+
+    {
+        id:1,
+        link:logo3
+    },
+    {
+        id:2,
+        link:logo1
+    },
+    {
+        id:3,
+        link:logo4
+    },
+    {
+        id:4,
+        link:logo2
+    },
+]
+
+
+export default imgdata;
