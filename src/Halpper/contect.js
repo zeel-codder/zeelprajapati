5a496a00b4dfc5f04144c3cb26590f088c061a6f
zeel-codder <zeelprajapati321@gmail.com>
2021-02-28 10:22:17 +0530

    adding gmail service

--- src/Halpper/contect.js
+++ src/Halpper/contect.js
@@ -1,11 +1,32 @@
 
+import{ init} from 'emailjs-com';
+import emailjs from 'emailjs-com';
 import {Link} from 'react-dom'
 import {AiTwotoneMail,AiTwotoneFileText} from 'react-icons/ai'
+import {BsFillPersonFill} from 'react-icons/bs'
 import {MdSubject} from 'react-icons/md'
 export default ()=>{
+    
+    const mailme=(e)=>{
+        e.preventDefault();
+        console.log(e.target);
+        init("user_T7VYY639xcXhhBCzLUUCo");
+        emailjs.sendForm('service_50van2j', 'template_ar6zjgw', e.target)
+      .then((result) => {
+          console.log(result.text);
+          alert("send message")
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+    }
 
     return (
-        <form className="form">
+        <form className="form" onSubmit={(e)=>{mailme(e)}}>
+            <div>
+            <label for="name"><BsFillPersonFill />Name:</label>
+            <input className="form-input" type="text" name="name" placeholder="Enter Name" required></input>
+            </div>
             <div>
             <label for="email"><AiTwotoneMail></AiTwotoneMail>Email:</label>
             <input className="form-input" type="email" name="email" placeholder="Enter Email" required></input>
